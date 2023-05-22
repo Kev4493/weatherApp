120a2f1a536a9e6b2b85d6961382517a9142308e
Kevin Wagner <kevin_wagner4493@web.de>
2023-05-22 21:17:16 +0200

    added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainScreenComponent } from './components/main-screen/main-screen.component';
+import { SearchScreenComponent } from './components/search-screen/search-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainScreenComponent },
+  { path: 'search', component: SearchScreenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
