e880926c6a1513951ca61d0f18ba730ebd4f3533
Kevin Wagner <kevin_wagner4493@web.de>
2023-08-15 18:21:00 +0200

    added background to main-screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainScreenComponent } from './components/main-screen/main-screen.component';
+import { StartScreenComponent } from './components/start-screen/start-screen.component';
 
 const routes: Routes = [
+  { path: 'start', component: StartScreenComponent },
   { path: '', component: MainScreenComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { MainScreenComponent } from './components/main-screen/main-screen.component';
 import { SideScreenComponent } from './components/side-screen/side-screen.component';
 import { HttpClientModule } from '@angular/common/http';
+import { StartScreenComponent } from './components/start-screen/start-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainScreenComponent,
     SideScreenComponent,
+    StartScreenComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
